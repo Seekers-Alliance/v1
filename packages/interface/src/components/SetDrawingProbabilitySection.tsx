049ede36ffc2bacfc61c816ee3b9c420b1824821
Wade <wade@teahouse.finance>
2023-12-04 06:57:16 +0800

    update probability section

--- packages/interface/src/components/SetDrawingProbabilitySection.tsx
+++ packages/interface/src/components/SetDrawingProbabilitySection.tsx
@@ -2,8 +2,11 @@ import EditPoolInput from '@/components/EditPoolInput';
 import { AddCard } from '@/components/AddCard';
 import AddProbabilityCard from '@/components/AddProbabilityCard';
 import { Primary2Button } from '@/components/Button';
-import React from 'react';
+import React, {useCallback, useMemo} from 'react';
 import ProbabilityInputCard from '@/components/ProbabilityInputCard';
+import {TOKEN_LIST} from "@/types";
+import {useUnitPoolStore} from "@/stores/unitPool";
+import {useDrawingPoolStore} from "@/stores/drawingPool";
 
 interface SetDrawingProbabilitySectionProps {
   poolName: string;
@@ -12,6 +15,26 @@ interface SetDrawingProbabilitySectionProps {
 export default function SetDrawingProbabilitySection({
   poolName,
 }: SetDrawingProbabilitySectionProps) {
+  const {getPool,add}=useDrawingPoolStore();
+  const {pools:unitPools}=useUnitPoolStore();
+  const poolProbabilityList=useMemo(
+      ()=>getPool(poolName)?.probabilities || Array.from({length:unitPools.list.length},()=>0),
+      [poolName])
+  const [probabilityList,setProbabilityList]=React.useState<number[]>(poolProbabilityList)
+  console.log(getPool(poolName))
+  const handleSetProbabilities=useCallback(()=>{
+        console.log('setProbabilities')
+        add({
+          name:poolName,
+          probabilities:probabilityList,
+        })
+        console.log(probabilityList)
+      },
+      [poolName,add,probabilityList])
+  const handleUpdateProbability=useCallback((index:number,value:number)=>{
+    probabilityList[index]=value
+    setProbabilityList(probabilityList)
+  },[probabilityList])
   return (
     <div>
       <div className='flex flex-col gap-4'>
@@ -22,13 +45,18 @@ export default function SetDrawingProbabilitySection({
           <div className='h-[122px] w-[110px]'>
             <AddCard>Add Unit</AddCard>
           </div>
-          {new Array(3).fill(0).map((_, index) => {
+          {unitPools.list.map((name, index) => {
             return (
               <div className='w-[110px]' key={index}>
-                <ProbabilityInputCard>
+                <ProbabilityInputCard defaultValue={poolProbabilityList[index]} onChange={(v)=>{
+                  if (v) {
+                    console.log(v)
+                    handleUpdateProbability(index,Number(v))
+                  }
+                }}>
                   <div className='mt-1 flex h-[76px] w-[100%] items-center px-2'>
                     <div className="font-['Source Sans Pro'] text-[14px] text-white">
-                      Unit Pool A Rares
+                      {name}
                     </div>
                   </div>
                 </ProbabilityInputCard>
@@ -37,7 +65,7 @@ export default function SetDrawingProbabilitySection({
           })}
         </div>
         <div className='w-[300px]'>
-          <Primary2Button>Set Probabilities</Primary2Button>
+          <Primary2Button onClick={handleSetProbabilities}>Set Probabilities</Primary2Button>
         </div>
       </div>
     </div>
--- packages/interface/src/components/SetUnitProbabilitySection.tsx
+++ packages/interface/src/components/SetUnitProbabilitySection.tsx
@@ -2,8 +2,10 @@ import EditPoolInput from '@/components/EditPoolInput';
 import { AddCard } from '@/components/AddCard';
 import AddProbabilityCard from '@/components/AddProbabilityCard';
 import { Primary2Button } from '@/components/Button';
-import React from 'react';
+import React, {useCallback, useMemo} from 'react';
 import ProbabilityInputCard from '@/components/ProbabilityInputCard';
+import {TOKEN_LIST} from "@/types";
+import {useUnitPoolStore} from "@/stores/unitPool";
 
 interface SetUnitProbabilitySectionProps {
   poolName: string;
@@ -12,6 +14,25 @@ interface SetUnitProbabilitySectionProps {
 export default function SetUnitProbabilitySection({
   poolName,
 }: SetUnitProbabilitySectionProps) {
+  const {getPool,add}=useUnitPoolStore();
+  const poolProbabilityList=useMemo(
+      ()=>getPool(poolName)?.probabilities || Array.from({length:TOKEN_LIST.length},()=>0),
+      [poolName])
+  const [probabilityList,setProbabilityList]=React.useState<number[]>(poolProbabilityList)
+  console.log(getPool(poolName))
+  const handleSetProbabilities=useCallback(()=>{
+        console.log('setProbabilities')
+      add({
+        name:poolName,
+        probabilities:probabilityList,
+      })
+        console.log(probabilityList)
+    },
+    [poolName,add,probabilityList])
+  const handleUpdateProbability=useCallback((index:number,value:number)=>{
+    probabilityList[index]=value
+    setProbabilityList(probabilityList)
+  },[probabilityList])
   return (
     <div>
       <div className='flex flex-col gap-4'>
@@ -22,12 +43,17 @@ export default function SetUnitProbabilitySection({
           <div className='h-[220px] w-[110px]'>
             <AddCard>Add Card</AddCard>
           </div>
-          {new Array(7).fill(0).map((_, index) => {
+          {TOKEN_LIST.map((item, index) => {
             return (
               <div className='h-52 w-[110px]' key={index}>
-                <ProbabilityInputCard>
+                <ProbabilityInputCard defaultValue={poolProbabilityList[index]} onChange={(v)=>{
+                  if (v) {
+                    console.log(v)
+                    handleUpdateProbability(index,Number(v))
+                  }
+                }}>
                   <div className="font-['Space Grotesk'] mt-1 w-[100%] px-2 text-[12px] font-bold uppercase text-white">
-                    ID-001
+                    {`ID-${item.toString().padStart(3, '0')}`}
                   </div>
                   <img src='/Bubble%20Gunner.png' />
                 </ProbabilityInputCard>
@@ -36,7 +62,7 @@ export default function SetUnitProbabilitySection({
           })}
         </div>
         <div className='w-[300px]'>
-          <Primary2Button>Set Probabilities</Primary2Button>
+          <Primary2Button onClick={handleSetProbabilities}>Set Probabilities</Primary2Button>
         </div>
       </div>
     </div>
