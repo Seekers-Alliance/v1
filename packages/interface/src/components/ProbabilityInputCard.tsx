a5bce5d49e781f8c1979afa736c2e610e9760632
Wade <wade@teahouse.finance>
2023-12-04 06:56:34 +0800

    add the input component in the ProbabilityInputCard.tsx

--- packages/interface/src/components/ProbabilityInputCard.tsx
+++ packages/interface/src/components/ProbabilityInputCard.tsx
@@ -1,12 +1,14 @@
-import { Card, Input } from 'antd';
+import {Card, Input, InputNumber} from 'antd';
 import styled from 'styled-components';
 
 interface ProbabilityInputCardProps {
-  children?: React.ReactNode;
+    defaultValue?: number;
+    onChange?: (value: number | string | null) => void;
+    children?: React.ReactNode;
 }
 
 export default function ProbabilityInputCard({
-  children,
+  children,onChange,defaultValue
 }: ProbabilityInputCardProps) {
   return (
     <BaseCard
@@ -21,12 +23,8 @@ export default function ProbabilityInputCard({
     >
       <div className='flex h-[100%] w-[100%] flex-col items-center justify-between'>
         {children}
-        {/*<div className="mt-1 font-['Space Grotesk'] w-[100%] px-2 text-[12px] font-bold uppercase text-white">*/}
-        {/*  ID-001*/}
-        {/*</div>*/}
-        {/*<img src='/Bubble%20Gunner.png' className='w-[110px]' />*/}
-        <div className='h-[40px] w-[100%] border-t-[1px] border-solid border-[#374151]'>
-          {/*<Input className="w-[100%] bg-[#111827] border-0"/>*/}
+        <div className='h-[40px] w-[100%] border-t-[1px] border-solid border-[#374151] flex items-center'>
+          <InputNumber className="w-[100%] bg-[#111827] border-0 text-white" onChange={onChange} defaultValue={defaultValue || 0}/>
         </div>
       </div>
     </BaseCard>
