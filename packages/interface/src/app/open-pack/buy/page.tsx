af488cb80f9fbee184027cda89caa6ddd42af383
Wade <wade@teahouse.finance>
2023-12-05 02:58:23 +0800

    fix: import error

--- packages/interface/src/app/open-pack/buy/page.tsx
+++ packages/interface/src/app/open-pack/buy/page.tsx
@@ -3,7 +3,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { PrimaryButton, SelectingButton } from '@/components/Button';
 import { useAccount, useNetwork, useSwitchNetwork } from 'wagmi';
 import { useRouter } from 'next/navigation';
-import ConnectWalletButton from '@/components/ConnectWalletButton';
+import {ConnectWalletButton} from '@/components/ConnectWalletButton';
 
 export default function Page() {
   const networkList = ['AVALANCHE', 'ETHEREUM', 'ARBITRUM'];
