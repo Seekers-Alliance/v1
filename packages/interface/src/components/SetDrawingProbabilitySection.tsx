a13cdce4887042474a4d140ba0380ca47426293e
Wade <wade@teahouse.finance>
2023-12-04 17:27:31 +0800

    update the set probability page

--- packages/interface/src/components/SetDrawingProbabilitySection.tsx
+++ packages/interface/src/components/SetDrawingProbabilitySection.tsx
@@ -1,12 +1,14 @@
 import EditPoolInput from '@/components/EditPoolInput';
 import { AddCard } from '@/components/AddCard';
-import AddProbabilityCard from '@/components/AddProbabilityCard';
-import { Primary2Button } from '@/components/Button';
-import React, {useCallback, useMemo} from 'react';
+import {SpecificChainButton} from '@/components/Button';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import ProbabilityInputCard from '@/components/ProbabilityInputCard';
-import {TOKEN_LIST} from "@/types";
-import {useUnitPoolStore} from "@/stores/unitPool";
-import {useDrawingPoolStore} from "@/stores/drawingPool";
+import { useUnitPoolStore } from '@/stores/unitPool';
+import { useDrawingPoolStore } from '@/stores/drawingPool';
+import useDrawingWrite from '@/hooks/useDrawingWrite';
+import useTxnNotify from '@/hooks/useTxnNotify';
+import { TransactionAction } from '@/components/transaction';
+import { useWaitForTransaction } from 'wagmi';
 
 interface SetDrawingProbabilitySectionProps {
   poolName: string;
@@ -15,59 +17,130 @@ interface SetDrawingProbabilitySectionProps {
 export default function SetDrawingProbabilitySection({
   poolName,
 }: SetDrawingProbabilitySectionProps) {
-  const {getPool,add}=useDrawingPoolStore();
-  const {pools:unitPools}=useUnitPoolStore();
-  const poolProbabilityList=useMemo(
-      ()=>getPool(poolName)?.probabilities || Array.from({length:unitPools.list.length},()=>0),
-      [poolName])
-  const [probabilityList,setProbabilityList]=React.useState<number[]>(poolProbabilityList)
-  console.log(getPool(poolName))
-  const handleSetProbabilities=useCallback(()=>{
-        console.log('setProbabilities')
-        add({
-          name:poolName,
-          probabilities:probabilityList,
-        })
-        console.log(probabilityList)
-      },
-      [poolName,add,probabilityList])
-  const handleUpdateProbability=useCallback((index:number,value:number)=>{
-    probabilityList[index]=value
-    setProbabilityList(probabilityList)
-  },[probabilityList])
+  const { getPool, add } = useDrawingPoolStore();
+  const { pools: unitPools } = useUnitPoolStore();
+  const poolProbabilityList = useMemo(
+    () =>
+      getPool(poolName)?.probabilities ||
+      Array.from({ length: unitPools.list.length }, () => 0),
+    [poolName]
+  );
+  const [probabilityList, setProbabilityList] =
+    React.useState<number[]>(poolProbabilityList);
+  console.log(getPool(poolName));
+  const { handleTxnResponse, contextHolder } = useTxnNotify();
+  const handleSetProbabilities = useCallback(() => {
+    console.log('setProbabilities');
+    // add({
+    //   name: poolName,
+    //   probabilities: probabilityList,
+    // });
+    setDrawing({
+      args: [0, probabilityList],
+    });
+    console.log(probabilityList);
+  }, [poolName, add, probabilityList]);
+  const {
+    data: setDrawingData,
+    write: setDrawing,
+    isError: isSetDrawingError,
+    isSuccess: isSetDrawingSuccess,
+    error: setDrawingError,
+  } = useDrawingWrite('setDrawing');
+  const {
+    isError: isSetDrawingTxnError,
+    error: setDrawingTxnError,
+    isSuccess: isSetDrawingTxnSuccess,
+  } = useWaitForTransaction({
+    hash: setDrawingData?.hash,
+    chainId: 43113,
+  });
+  const handleUpdateProbability = useCallback(
+    (index: number, value: number) => {
+      probabilityList[index] = value;
+      setProbabilityList(probabilityList);
+    },
+    [probabilityList]
+  );
+  useEffect(() => {
+    handleTxnResponse(
+      TransactionAction.SUBMIT,
+      isSetDrawingError,
+      isSetDrawingSuccess,
+      setDrawingError
+    );
+  }, [isSetDrawingError, isSetDrawingSuccess, setDrawingError]);
+  useEffect(() => {
+    handleTxnResponse(
+      TransactionAction.CONFIRM,
+      isSetDrawingError,
+      isSetDrawingSuccess,
+      setDrawingError
+    );
+    if (isSetDrawingTxnSuccess) {
+      add({
+        name: poolName,
+        probabilities: probabilityList,
+      });
+    }
+  }, [
+    isSetDrawingTxnError,
+    isSetDrawingTxnSuccess,
+    setDrawingTxnError,
+    poolName,
+    probabilityList,
+  ]);
+
+  useEffect(() => {
+    if (isSetDrawingTxnSuccess) {
+      add({
+        name: poolName,
+        probabilities: probabilityList,
+      });
+    }
+  }, [isSetDrawingTxnSuccess]);
+
   return (
-    <div>
-      <div className='flex flex-col gap-4'>
-        <div className='w-[300px]'>
-          <EditPoolInput>{poolName}</EditPoolInput>
-        </div>
-        <div className='grid grid-flow-dense grid-cols-8 gap-2'>
-          <div className='h-[122px] w-[110px]'>
-            <AddCard>Add Unit</AddCard>
+    <>
+      {contextHolder}
+      <div>
+        <div className='flex flex-col gap-4'>
+          <div className='w-[300px]'>
+            <EditPoolInput>{poolName}</EditPoolInput>
           </div>
-          {unitPools.list.map((name, index) => {
-            return (
-              <div className='w-[110px]' key={index}>
-                <ProbabilityInputCard defaultValue={poolProbabilityList[index]} onChange={(v)=>{
-                  if (v) {
-                    console.log(v)
-                    handleUpdateProbability(index,Number(v))
-                  }
-                }}>
-                  <div className='mt-1 flex h-[76px] w-[100%] items-center px-2'>
-                    <div className="font-['Source Sans Pro'] text-[14px] text-white">
-                      {name}
+          <div className='grid grid-flow-dense grid-cols-8 gap-2'>
+            <div className='h-[122px] w-[110px]'>
+              <AddCard>Add Unit</AddCard>
+            </div>
+            {unitPools.list.map((name, index) => {
+              return (
+                <div className='w-[110px]' key={index}>
+                  <ProbabilityInputCard
+                    defaultValue={poolProbabilityList[index]}
+                    onChange={(v) => {
+                      if (v) {
+                        console.log(v);
+                        handleUpdateProbability(index, Number(v));
+                      }
+                    }}
+                  >
+                    <div className='mt-1 flex h-[76px] w-[100%] items-center px-2'>
+                      <div className="font-['Source Sans Pro'] text-[14px] text-white">
+                        {name}
+                      </div>
                     </div>
-                  </div>
-                </ProbabilityInputCard>
-              </div>
-            );
-          })}
-        </div>
-        <div className='w-[300px]'>
-          <Primary2Button onClick={handleSetProbabilities}>Set Probabilities</Primary2Button>
+                  </ProbabilityInputCard>
+                </div>
+              );
+            })}
+          </div>
+          <div className='w-[300px]'>
+            <SpecificChainButton chainId={43113} onClick={handleSetProbabilities}>
+              Set Probabilities
+            </SpecificChainButton>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- packages/interface/src/components/SetUnitProbabilitySection.tsx
+++ packages/interface/src/components/SetUnitProbabilitySection.tsx
@@ -1,11 +1,14 @@
 import EditPoolInput from '@/components/EditPoolInput';
 import { AddCard } from '@/components/AddCard';
-import AddProbabilityCard from '@/components/AddProbabilityCard';
-import { Primary2Button } from '@/components/Button';
-import React, {useCallback, useMemo} from 'react';
+import {SpecificChainButton} from '@/components/Button';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import ProbabilityInputCard from '@/components/ProbabilityInputCard';
-import {TOKEN_LIST} from "@/types";
-import {useUnitPoolStore} from "@/stores/unitPool";
+import { TOKEN_LIST } from '@/types';
+import { useUnitPoolStore } from '@/stores/unitPool';
+import { TransactionAction } from '@/components/transaction';
+import useTxnNotify from '@/hooks/useTxnNotify';
+import NFTProfile from '@/components/NFTProfile';
+import useDrawingTxn from '@/hooks/useDrawingTxn';
 
 interface SetUnitProbabilitySectionProps {
   poolName: string;
@@ -14,57 +17,104 @@ interface SetUnitProbabilitySectionProps {
 export default function SetUnitProbabilitySection({
   poolName,
 }: SetUnitProbabilitySectionProps) {
-  const {getPool,add}=useUnitPoolStore();
-  const poolProbabilityList=useMemo(
-      ()=>getPool(poolName)?.probabilities || Array.from({length:TOKEN_LIST.length},()=>0),
-      [poolName])
-  const [probabilityList,setProbabilityList]=React.useState<number[]>(poolProbabilityList)
-  console.log(getPool(poolName))
-  const handleSetProbabilities=useCallback(()=>{
-        console.log('setProbabilities')
-      add({
-        name:poolName,
-        probabilities:probabilityList,
-      })
-        console.log(probabilityList)
+  const { getPool, add } = useUnitPoolStore();
+  const poolProbabilityList = useMemo(
+    () =>
+      getPool(poolName)?.probabilities ||
+      Array.from({ length: TOKEN_LIST.length }, () => 0),
+    [poolName]
+  );
+  const [probabilityList, setProbabilityList] =
+    React.useState<number[]>(poolProbabilityList);
+  const { handleTxnResponse, contextHolder } = useTxnNotify();
+  const {
+    hash,
+    submit,
+    isSubmitError,
+    isSubmitSuccess,
+    submitError,
+    confirmError,
+    isConfirmSuccess,
+    isConfirmError,
+  } = useDrawingTxn('setAtomic');
+  console.log(getPool(poolName));
+  const handleSetProbabilities = useCallback(() => {
+    console.log('setProbabilities');
+    // add({
+    //   name: poolName,
+    //   probabilities: probabilityList,
+    // });
+    submit?.({ args: [1, probabilityList] });
+  }, [poolName, add, probabilityList]);
+  const handleUpdateProbability = useCallback(
+    (index: number, value: number) => {
+      probabilityList[index] = value;
+      setProbabilityList(probabilityList);
     },
-    [poolName,add,probabilityList])
-  const handleUpdateProbability=useCallback((index:number,value:number)=>{
-    probabilityList[index]=value
-    setProbabilityList(probabilityList)
-  },[probabilityList])
+    [probabilityList]
+  );
+
+  useEffect(() => {
+    handleTxnResponse(
+      TransactionAction.SUBMIT,
+      isSubmitError,
+      isSubmitSuccess,
+      submitError
+    );
+  }, [isSubmitError, isSubmitSuccess, submitError]);
+  useEffect(() => {
+    handleTxnResponse(
+      TransactionAction.CONFIRM,
+      isConfirmError,
+      isConfirmSuccess,
+      confirmError
+    );
+  }, [isConfirmError, isConfirmSuccess, confirmError]);
+  useEffect(() => {
+    if (isConfirmSuccess) {
+      add({
+        name: poolName,
+        probabilities: probabilityList,
+      });
+    }
+  }, [isConfirmSuccess]);
   return (
-    <div>
-      <div className='flex flex-col gap-4'>
-        <div className='w-[300px]'>
-          <EditPoolInput>{poolName}</EditPoolInput>
-        </div>
-        <div className='grid grid-flow-dense grid-cols-8 gap-2'>
-          <div className='h-[220px] w-[110px]'>
-            <AddCard>Add Card</AddCard>
+    <>
+      {contextHolder}
+      <div>
+        <div className='flex flex-col gap-4'>
+          <div className='w-[300px]'>
+            <EditPoolInput>{poolName}</EditPoolInput>
           </div>
-          {TOKEN_LIST.map((item, index) => {
-            return (
-              <div className='h-52 w-[110px]' key={index}>
-                <ProbabilityInputCard defaultValue={poolProbabilityList[index]} onChange={(v)=>{
-                  if (v) {
-                    console.log(v)
-                    handleUpdateProbability(index,Number(v))
-                  }
-                }}>
-                  <div className="font-['Space Grotesk'] mt-1 w-[100%] px-2 text-[12px] font-bold uppercase text-white">
-                    {`ID-${item.toString().padStart(3, '0')}`}
-                  </div>
-                  <img src='/Bubble%20Gunner.png' />
-                </ProbabilityInputCard>
-              </div>
-            );
-          })}
-        </div>
-        <div className='w-[300px]'>
-          <Primary2Button onClick={handleSetProbabilities}>Set Probabilities</Primary2Button>
+          <div className='grid grid-flow-dense grid-cols-8 gap-2'>
+            <div className='h-[220px] w-[110px]'>
+              <AddCard>Add Card</AddCard>
+            </div>
+            {TOKEN_LIST.map((item, index) => {
+              return (
+                <div className='h-52 w-[110px]' key={index}>
+                  <ProbabilityInputCard
+                    defaultValue={poolProbabilityList[index]}
+                    onChange={(v) => {
+                      if (v) {
+                        console.log(v);
+                        handleUpdateProbability(index, Number(v));
+                      }
+                    }}
+                  >
+                    <NFTProfile tokenId={item} />
+                  </ProbabilityInputCard>
+                </div>
+              );
+            })}
+          </div>
+          <div className='w-[300px]'>
+            <SpecificChainButton chainId={43113} onClick={handleSetProbabilities}>
+              Set Probabilities
+            </SpecificChainButton>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
