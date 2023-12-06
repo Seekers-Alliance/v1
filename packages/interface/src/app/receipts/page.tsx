dab82c4517c15382c8c1c041a62fc70b8599c561
Wade <wade@teahouse.finance>
2023-12-06 11:29:28 +0800

    update the NFTProfile

--- packages/interface/src/app/receipts/page.tsx
+++ packages/interface/src/app/receipts/page.tsx
@@ -3,27 +3,52 @@ import NFTCard from '@/components/NFTCard';
 import { NavButton } from '@/components/Button';
 import SimpleNavbar from '@/components/SimpleNavbar';
 import { useRouter } from 'next/navigation';
+import Icon from "@ant-design/icons";
+import Link from "next/link";
+import {useCallback} from "react";
+import {NFTProfile2} from "@/components/NFTProfile";
 
 export default function Page() {
   const router = useRouter();
-
+  const toOpensea=useCallback(() => {
+            router.push('https://testnets.opensea.io/zh-TW/collection/seekers-alliance');
+        }, [router],
+  )
+  const toVRF=useCallback(() => {
+            router.push('https://vrf.chain.link/fuji/822');
+        },[router])
   return (
     <main className='min-h-screen bg-black bg-[url("/repository.png")]'>
       <div className='fixed left-[131px] top-[50px] w-[220px]'>
         <SimpleNavbar />
       </div>
-      <div className='fixed left-[849px] top-[51px]'>
-        <div className='h-[60px] w-[463px]'>
-          <NavButton>
+      <div className='fixed left-[650px] top-[51px]'>
+          <div className='flex flex-row gap-4'>
+          <div className='h-[60px] w-[250px]'>
+              <NavButton onClick={toOpensea}>
+                  <div className='inline-flex items-center gap-4'>
+                      <span>View NFTs on OpenSea</span>
+                      <span>
+                  <Icon
+                      component={() => <img src={'/External-Link-1.svg'} width={25} height={20} />}
+                  />
+                </span>
+                  </div>
+              </NavButton>
+          </div>
+        <div className='h-[60px] w-[420px]'>
+          <NavButton onClick={toVRF}>
             <div className='inline-flex items-center gap-4'>
               <span>CHAINLINK VRF TRANSACTION ID TO PROVE FAIRNESS</span>
-              {/*<span>*/}
-              <span className='flex items-center justify-center rounded-[4px]'>
-                <img src={'/External-Link-1.svg'} width={36} height={36} />
-              </span>
+              <span>
+                  <Icon
+                      component={() => <img src={'/External-Link-1.svg'} width={25} height={20} />}
+                  />
+                </span>
             </div>
           </NavButton>
         </div>
+          </div>
       </div>
       <div className='w-[100%] p-[193px]'>
         <div className='grid grid-flow-dense grid-cols-5 justify-between'>
@@ -31,15 +56,7 @@ export default function Page() {
             .fill(0)
             .map((_, index) => (
               <div key={index} className='flex flex-col items-center'>
-                <NFTCard width={500} height={500} />
-                <div className=''>
-                  <span className="font-['Space Grotesk'] text-[22px] font-normal uppercase text-white">
-                    ID{' '}
-                  </span>
-                  <span className="font-['Space Grotesk'] text-[22px] font-bold uppercase text-white">
-                    100
-                  </span>
-                </div>
+                  <NFTProfile2 tokenId={BigInt(1)} />
               </div>
             ))}
         </div>
