9b9649158e5c9251ed60d0df04d04a3f2e6d38d4
Wade <wade@teahouse.finance>
2023-12-07 14:12:30 +0800

    update the theme

--- packages/interface/src/theme/themeConfig.ts
+++ packages/interface/src/theme/themeConfig.ts
@@ -4,8 +4,9 @@ const theme: ThemeConfig = {
   token: {
     fontSize: 16,
     colorPrimary: '#1890ff',
-    // colorText: '#FFFFFF',
-    // colorBgElevated: 'rgb(75 85 99)',
+    colorText: '#FFFFFF',
+    colorBgElevated:'rgb(75 85 99)',
+    colorSplit: '#f0f0f0',
   },
   components: {
     Modal: {
